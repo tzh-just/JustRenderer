ab8d01e5ebf9ea9b2c4ecf9d58eb064ba82a8a29
Justian <just.tzh@outlook.com>
2022-08-14 21:56:15 +0800

    修复Scene文件

--- JustLab/Hittable/Scene.h
+++ JustLab/Hittable/Scene.h
@@ -6,10 +6,13 @@
 
 #include <vector>
 #include "Global.h"
-#include <Mesh.h"
+#include "Emitter/Emitter.h"
+#include "Mesh.h"
+
 
 namespace Just{
     struct Scene{
-        std::vector>
+        std::vector<std::shared_ptr<Mesh>> meshes;
+        std::vector<std::shared_ptr<Emitter>> emitters;
     };
 }
