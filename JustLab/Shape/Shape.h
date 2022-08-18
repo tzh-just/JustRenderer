c64c96faccad21fbf347b9cca855af314bd2da3a
Justian <just.tzh@outlook.com>
2022-08-18 22:27:52 +0800

    增加Shape父类

---
+++ JustLab/Shape/Shape.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Geometry/Transform.h"
+#include "Geometry/Bounds3.h"
+
+namespace Just {
+    struct Shape {
+        std::shared_ptr<Transform> objToWorld;
+        std::shared_ptr<Transform> worldToObj;
+
+        Shape(const std::shared_ptr<Transform>& objToWorld, const std::shared_ptr<Transform>& worldToObj)
+                : objToWorld(objToWorld), worldToObj(worldToObj) {}
+
+        virtual Bounds3f ObjectBound() const = 0;
+
+        virtual Bounds3f WorldBound() const = 0;
+    };
+}
