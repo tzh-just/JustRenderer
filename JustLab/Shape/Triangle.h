a8b3ec2cf544444499cc9487f1ab97b3356c1967
Justian <just.tzh@outlook.com>
2022-08-24 14:43:28 +0800

    参考pbrt-v3重构项目

---
+++ JustLab/Shape/Triangle.h
@@ -0,0 +1,8 @@
+//
+// Created by Justin on 2022/8/24.
+//
+
+#ifndef JUST_TRIANGLE_H
+#define JUST_TRIANGLE_H
+
+#endif //JUST_TRIANGLE_H
---
+++ JustLab/Shape/TriangleMesh.h
@@ -0,0 +1,8 @@
+//
+// Created by Justin on 2022/8/24.
+//
+
+#ifndef JUST_TRIANGLEMESH_H
+#define JUST_TRIANGLEMESH_H
+
+#endif //JUST_TRIANGLEMESH_H
